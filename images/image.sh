a0b72a3073f926f19355c5185eaad5e40186d53b
Marius Orcsik <marius@federated.id>
2022-12-09 18:59:35 +0800

    Adding support for building podman images in CI

---
+++ images/image.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+#set -x
+
+_environment=${ENV:-dev}
+_hostname=${APP_HOSTNAME:-point}
+_listen_port=${PORT:-3003}
+_version=${VERSION}
+
+_image_name=${1:-point:${_environment}}
+_build_name=${2:-localhost/point/builder}
+
+_builder=$(buildah from "${_build_name}":latest)
+if [[ -z "${_builder}" ]]; then
+    echo "Unable to find builder image: ${_build_name}"
+    exit 1
+fi
+
+echo "Building image ${_image_name} for host=${_hostname} env:${_environment} port:${_listen_port}"
+
+buildah run "${_builder}" make ENV="${_environment}" all
+buildah run "${_builder}" ./images/gen-certs.sh point
+
+_image=$(buildah from gcr.io/distroless/static:latest)
+
+buildah config --env ENV="${_environment}" "${_image}"
+buildah config --env APP_HOSTNAME="${_hostname}" "${_image}"
+buildah config --env LISTEN=:"${_listen_port}" "${_image}"
+buildah config --env KEY_PATH=/etc/ssl/certs/brutalinks.key "${_image}"
+buildah config --env CERT_PATH=/etc/ssl/certs/brutalinks.crt "${_image}"
+buildah config --env HTTPS=true "${_image}"
+
+buildah config --port "${_listen_port}" "${_image}"
+
+buildah config --volume /storage "${_image}"
+buildah config --volume /.env "${_image}"
+
+buildah copy --from "${_builder}" "${_image}" /go/src/app/bin/* /bin/
+buildah copy --from "${_builder}" "${_image}" /go/src/app/point.key /etc/ssl/certs/
+buildah copy --from "${_builder}" "${_image}" /go/src/app/point.crt /etc/ssl/certs/
+buildah copy --from "${_builder}" "${_image}" /go/src/app/point.pem /etc/ssl/certs/
+
+buildah config --workingdir / "${_image}"
+buildah config --entrypoint '["/bin/point"]' "${_image}"
+
+# commit
+buildah commit "${_image}" "${_image_name}"
