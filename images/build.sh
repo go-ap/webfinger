904d40872c31c77bef2794ad20eb41c32ab1dba5
Marius Orcsik <marius@federated.id>
2025-02-15 19:46:49 +0100

    Use golang alpine image v1.24 for container creation

--- images/build.sh
+++ images/build.sh
@@ -7,12 +7,14 @@ _image_name=${2:-point/builder}
 
 _context=$(realpath "${_workdir}")
 
-_builder=$(buildah from docker.io/library/golang:1.24rc3)
+_builder=$(buildah from docker.io/library/golang:1.24-alpine)
 
 buildah config --env GO111MODULE=on "${_builder}"
 buildah config --env GOWORK=off "${_builder}"
 
 buildah copy --ignorefile "${_context}/.containerignore" --contextdir "${_context}" "${_builder}" "${_context}" /go/src/app
+
+buildah run "${_builder}" apk add make bash openssl
 
 buildah config --workingdir /go/src/app "${_builder}"
 
