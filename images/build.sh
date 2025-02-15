3b8dd6ba346f9a3a3b6672f083e09a654ae50748
Marius Orcsik <marius@federated.id>
2025-02-15 20:07:58 +0100

    Import improvements form oni image build script

--- images/build.sh
+++ images/build.sh
@@ -9,16 +9,16 @@ _context=$(realpath "${_workdir}")
 
 _builder=$(buildah from docker.io/library/golang:1.24-alpine)
 
+buildah run "${_builder}" /sbin/apk update
+buildah run "${_builder}" /sbin/apk add make bash openssl
+
 buildah config --env GO111MODULE=on "${_builder}"
 buildah config --env GOWORK=off "${_builder}"
 
 buildah copy --ignorefile "${_context}/.containerignore" --contextdir "${_context}" "${_builder}" "${_context}" /go/src/app
-
-buildah run "${_builder}" apk add make bash openssl
-
 buildah config --workingdir /go/src/app "${_builder}"
 
-buildah run "${_builder}" make download
+buildah run "${_builder}" make go.sum
 buildah run "${_builder}" go mod vendor
 
 buildah commit "${_builder}" "${_image_name}"
